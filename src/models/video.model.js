a13275c11e35894f3a6e70ddda7d3a40896de655
HIMANSHU JAWLA <101585686+Codex-jawla@users.noreply.github.com>
2025-09-23 15:44:08 +0530

    till models

---
+++ src/models/video.model.js
@@ -0,0 +1,18 @@
+import mongoose, { Aggregate } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema =new mongoose.Schema({
+    videoFIle:{type:String,required:true},
+    thumbnail:{type:String,required:true},
+    title:{type:String,required:true},
+    discription:{type:String,required:true},
+    duration:{type:Number,required:true},
+    views:{type:Number,default:0},
+    ispublish:{type:Boolean,default:true},
+    owner:[{type:mongoose.Schema.Types.ObjectId,ref:"User"}],
+
+},{timestamps:true})
+
+
+videoSchema.plugin(mongooseAggregatePaginate);
+export const Video = mongoose.model("Video",videoSchema);
